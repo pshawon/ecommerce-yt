3894d1b5841577149852d6cdcee574ee086a353b
pshawon <shawon.ruet.12@gmail.com>
2025-07-01 22:57:09 +0600

    feat: controllers,routes,models initialised

---
+++ controllers/controllers.go
@@ -0,0 +1,34 @@
+package controllers
+
+import(
+
+)
+
+func HashPassword(password string) string{
+
+
+}
+
+func VerifyPassword(userPassword srting, givenPassword string) (bool, string ) {
+
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	
+}
---
+++ database/cart.go
@@ -0,0 +1,25 @@
+import controllers 
+
+import (
+
+)
+
+func AddToCart() gin.HandlerFunc{
+
+}
+
+func RemoveItem() gin.HandlerFunc{
+
+}
+
+func GetItemFromCart() gin.HandlerFunc{
+
+}
+
+func BuyFromCart() gin.HandlerFunc{
+	
+}
+
+func InstantBuy() gin.HandlerFunc{
+	
+}
